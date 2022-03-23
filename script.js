3830d525aa45ab9dc547e104671eab9a6b785da0
Afran <Jvelez13Soto@outlook.com>
2022-03-23 01:14:08 -0600

    mrgadget3d

---
+++ script.js
@@ -0,0 +1,21 @@
+var menu = document.querySelector(".Menu_v2");
+var submenu = document.querySelector(".m2");
+var checkbox = document.querySelector("input[name=mode]");
+
+
+
+
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+let trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
